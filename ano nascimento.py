0bf92e00b485244ac3982e423d19d995685d4190
Yumiixd <mariavitoriaxd27@gmail.com>
2026-06-29 16:19:59 -0300

    Upload dos exercícios de lógica em Python
    Adicionando exercícios de lógica de programação desenvolvidos durante os estudos de Python.

---
+++ ano nascimento.py
@@ -0,0 +1,7 @@
+# desafio 02 Você nao parece velho(a)
+
+ano_atual = 2025
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+idade = ano_atual - ano_nascimento
+
+print(f"Você tem {idade} anos!")
