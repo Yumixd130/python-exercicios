5dde01b5df366ead0f0e135d9858c7d3ab6b870d
Yumiixd <mariavitoriaxd27@gmail.com>
2026-06-29 16:32:20 -0300

    Atualizando codigo

--- calculadora_soma.py
+++ calculadora_soma.py
@@ -4,4 +4,4 @@ numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo numero: "))
 resultado = numero1 + numero2
 
-print("o resultado da conta" + str(resultado))
+print(f"O resultado da soma é: {resultado}")
